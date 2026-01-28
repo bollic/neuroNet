9b89568c4e1c73b2ffa916620fbfc8294a13bd02
bollic <soniaboselli@gmail.com>
2026-01-29 00:24:40 +0100

    Rimuovo Stripe

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Stripe = require("stripe");
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY); // chiave privata dal .env
+//const Stripe = require("stripe");
+//const stripe = Stripe(process.env.STRIPE_SECRET_KEY); // chiave privata dal .env
 
 router.post("/create-checkout-session", async (req, res) => {
   const { priceId } = req.body;
